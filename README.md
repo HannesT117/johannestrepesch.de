3c4cf89f25d2c49046d58b71794ec5d740c6bb8e
Johannes Trepesch <web-git@johannestrepesch.de>
2019-07-07 21:01:32 +0200

    build: configure travis

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # johannestrepesch.de
+
+[![Build Status](https://travis-ci.com/HannesT117/johannestrepesch.de.svg?branch=master)](https://travis-ci.com/HannesT117/johannestrepesch.de)
---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ev
+
+deploy_assets_to_server () {
+  dist_folder=$1
+
+  echo "Start deploying assets to remote."
+
+  export SSHPASS=$deploy_password
+  sshpass -e scp -o stricthostkeychecking=no -r $dist_folder $deploy_user@$deploy_host:$deploy_path
+  export SSHPASS=
+}
+
+echo "Starting deployment."
+
+if [ ! -d "$build_dir" ]; then
+  echo "ERROR: The build directory does not exist."
+  exit 1
+fi
+
+deploy_assets_to_server $build_dir
